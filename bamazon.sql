c9b3b2a50d7fa27a4cee3ff95e1b01715b97806b
jhanisusM <jhanisus@gmail.com>
2018-05-17 17:17:29 -0700

    seeding files

---
+++ bamazon.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE bamazonB;
+USE bamazonDB;
+
+CREATE TABLE products(
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NULL,
+    department_name VARCHAR (30) NULL,
+    product_description VARCHAR (250) NOT NULL,
+    price DECIMAL (10,2) INT NULL,
+    stock_quantity INTEGER, 
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
+INSERT INTO products (product_name, department_name, product_description ,price, stock_quantity) VALUES ("iPhone 6s+","Phones", "Best Phone ever!", 300.00, 100);
