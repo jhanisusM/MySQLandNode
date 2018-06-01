e1f5da357a766da75978fd213bf6230e251e617a
jhanisusM <jhanisus@gmail.com>
2018-05-31 19:15:01 -0700

    connection is modular

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var connection = require("./connection");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "cali",
-    database: "bamazonDB"
-});
 
 connection.connect(function (err) {
     if (err) throw err;
